551d0d21a130355329fca8affb9192996bb6d105
Gatsbyz_sc <zhousichen06@gmail.com>
2023-03-14 05:06:44 +1100

    html partial modify & switch sign-register

--- frontend/src/login_regis.js
+++ frontend/src/login_regis.js
@@ -1,50 +1,54 @@
 export function login() {
-    // need to be modified later for fetch
-    const eamilField = document.getElementById("email").value;
-    console.log(eamilField);
-    const passwordField = document.getElementById("password").value;
-    console.log(passwordField);
+  // need to be modified later for fetch
+  const eamilField = document.getElementById("email").value;
+  console.log(eamilField);
+  const passwordField = document.getElementById("password").value;
+  console.log(passwordField);
 
-    fetch(`http://localhost:5005/auth/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify({
-            email: "betty@email.com",
-            password: "cardigan",
-            // email: eamilField,
-            // password: passwordField,
-        }),
+  fetch(`http://localhost:5005/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      email: "betty@email.com",
+      password: "cardigan",
+      // email: eamilField,
+      // password: passwordField,
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
     })
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then((data) => {
-            console.log(data.token);
-            console.log(data.userId);
-        })
-        .catch(() => {
-            errorPopup(
-                "Your email and password don't match!!! Please try again"
-            );
-        });
+    .then((data) => {
+      console.log(data.token);
+      console.log(data.userId);
+    })
+    .catch(() => {
+      errorPopup("Your email and password don't match!!! Please try again");
+    });
 }
 
 export function registration() {
-    console.log("hello world");
+  console.log("hello world");
 }
 
 export function errorPopup(errorMessage = "error happens") {
-    // for testing this function, please modify the predefined value in email/password field
-    document.getElementById("error-message").textContent = errorMessage;
-    const errorBox = document.getElementById("error-box");
-    errorBox.classList.remove("hidden");
-    const closeButton = document.getElementById("close-error-window");
-    closeButton.addEventListener("click", () => {
-        errorBox.classList.add("hidden");
-    });
-    // resume from error
+  // for testing this function, please modify the predefined value in email/password field
+  document.getElementById("error-message").textContent = errorMessage;
+  const errorBox = document.getElementById("error-box");
+  errorBox.classList.remove("hidden");
+  const closeButton = document.getElementById("close-error-window");
+  closeButton.addEventListener("click", () => {
+    errorBox.classList.add("hidden");
+  });
+  // resume from error
+}
+
+// sign in switch register
+export function swap(page1, page2) {
+  document.getElementById(page1).classList.add("hidden");
+  document.getElementById(page2).classList.remove("hidden");
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,19 @@ import { BACKEND_PORT } from "./config.js";
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from "./helpers.js";
 
-import { login, registration, errorPopup } from "./login_regis.js";
+import { login, registration, errorPopup, swap } from "./login_regis.js";
 
 let signInButton = document.getElementById("sign-in-button");
 
 signInButton.addEventListener("click", () => {
-    login();
+  login();
+});
+
+// sign in switch register
+document.getElementById("switch-join-btn").addEventListener("click", () => {
+  swap("login-interface", "registration-interface");
+});
+
+document.getElementById("switch-sign-btn").addEventListener("click", () => {
+  swap("registration-interface", "login-interface");
 });
